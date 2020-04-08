c8bc1bd2adc8f88797ad3f131c6dab4ec30448d0
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-04-07 21:15:57 -0500

    Decipher encoded message

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 # Resolve the problem!!
-
+import re
 
 def run():
     # Start coding here
-
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
+        x = re.findall('[a-z]',text)
+        print(' '.join([str(elem) for elem in x]))
+        
 
 if __name__ == '__main__':
     run()
